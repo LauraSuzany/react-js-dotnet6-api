3da9a86bc3b20908a02f56d9da38b00ef421ca3f
Laura Suzany <108367864+LauraVoxData@users.noreply.github.com>
2023-03-10 19:20:00 -0300

    Feat: Implementando ações porém está com erro na (rota)

--- client/src/pages/Books/index.js
+++ client/src/pages/Books/index.js
@@ -1,63 +1,95 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './styles.css';
 import logoImage from '../../assets/logo.svg';
 import { FiPower, FiEdit, FiTrash2 } from 'react-icons/fi'
+import {useNavigate} from 'react-router-dom';
+import api from '../../services/api';
+
+//()=>DeleteBook(book.id) é para impedir que apague todos os regostros de uma vez ao recarregar a pagina(?)
 
 export default function Books() {
+
+  const userName = localStorage.getItem('userName');
+  const accessToken = localStorage.getItem('accessToken')
+  const [books, setbooks] = useState([]);
+  const history = useNavigate();
+
+  useEffect(()=>{
+      api.get('/api/Book/v1/asc/20/1', {
+        headers: {
+          Authorization: `Bearer ${accessToken}` 
+        }
+      }).then(response => {setbooks(response.data.list)
+        })
+  }, [accessToken]);
+
+  async function Logout(){
+    try {
+      await api.get(`/api/auth/v1/revoke`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`}
+        });
+        localStorage.clear();
+        history('/');
+    }catch(error){
+      alert('Logout fieled! Try Again.')
+    }
+  }
+
+  async function EditBook(id){
+    try {
+     history(`book/new/${id}`)
+    }catch(error){
+      alert('Edit book fieled! Try Again.')
+    }
+  }
+
+  async function DeleteBook(id){
+    try {
+      await api.delete(`/api/Book/v1/${id}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`}
+        });
+
+        setbooks(books.filter(book => book.id !== id))
+    }catch(error){
+      alert('Delete fieled! Try Again.')
+    }
+  }
+
   return (
     <div className="book-container">
       <header>
         <img src={logoImage} alt="logo" />
-        <span>Bem-vinda, <strong>Laura</strong>!</span>
-        <a className="button" href="/book/new">Adicionar novo livro</a>
+        <span>Bem-vinda, <strong>{userName.toUpperCase()}</strong>!</span>
+        <a className="button" href="/book/new/0">Adicionar novo livro</a>
         <button type="button">
-          <FiPower size={18} color="#251fCS5"/>
+          <FiPower onClick={Logout} size={18} color="#251fCS5"/>
         </button>
       </header>
       <h1>Register books</h1>
       <ul>
-        <li>
+        {books.map(book=> (
+        <li key={book.id}>
           <strong>Title:</strong>
-          <p>Docker deep dive</p>
+          <p>{book.title}</p>
           <strong>Autor:</strong>
-          <p>Nigel Poult</p>
+          <p>{book.author}</p>
           <strong>Price:</strong>
-          <p>R$47,90</p>
+          <p>{Intl.NumberFormat('pt-BR', {style:'currency', currency:'BRL'}).format(book.price)}</p>
           <strong>Release Date:</strong>
-          <p>12/07/2007</p>
+          <p>{Intl.DateTimeFormat('pt-BR').format(new Date(book.launchDate))}</p>
+
           <button type="button"> 
-            <FiEdit size={20} color="#251FC5"/>
-            <FiTrash2 size={20} color="#251FC5"/>
+          <FiEdit onClick={() => EditBook(book.id)} size={20} color="#251FC5"/>
           </button>
-        </li>
-        <li>
-          <strong>Title:</strong>
-          <p>Docker deep dive</p>
-          <strong>Autor:</strong>
-          <p>Nigel Poult</p>
-          <strong>Price:</strong>
-          <p>R$47,90</p>
-          <strong>Release Date:</strong>
-          <p>12/07/2007</p>
-          <button type="button"> 
-          <FiEdit size={20} color="#251FC5"/>
+          
+          <button onClick={()=>DeleteBook(book.id)}>
           <FiTrash2 size={20} color="#251FC5"/>
           </button>
+
         </li>
-        <li>
-          <strong>Title:</strong>
-          <p>Docker deep dive</p>
-          <strong>Autor:</strong>
-          <p>Nigel Poult</p>
-          <strong>Price:</strong>
-          <p>R$47,90</p>
-          <strong>Release Date:</strong>
-          <p>12/07/2007</p>
-          <button type="button"> 
-          <FiEdit size={20} color="#251FC5"/>
-          <FiTrash2 size={20} color="#251FC5"/>
-          </button>
-        </li>
+        ))}
       </ul>
     </div>
   );
